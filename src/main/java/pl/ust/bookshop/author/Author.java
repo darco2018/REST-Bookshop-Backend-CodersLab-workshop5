aa930bcd10c88a2b1b50dd479c8167499a8db44d
darco2018 <us3cki@zoho.com>
2018-06-20 13:41:08 +0200

    Add relation to Book in Author

--- src/main/java/pl/ust/bookshop/author/Author.java
+++ src/main/java/pl/ust/bookshop/author/Author.java
@@ -3,15 +3,18 @@ package pl.ust.bookshop.author;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Index;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.annotations.Where;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,7 +23,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import pl.ust.bookshop.book.Book;
+import pl.ust.bookshop.authorbook.AuthorBook;
 import pl.ust.bookshop.model.BaseEntity;
 
 @Getter @Setter @Builder @AllArgsConstructor  @NoArgsConstructor
@@ -46,11 +49,19 @@ public class Author extends BaseEntity {
 	
 	@NaturalId(mutable = true)
 	private String email;
+	
+	@JsonIgnore // prevents recursion when calling books
+	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<AuthorBook> books = new ArrayList<>();
 
-	
-	@ManyToMany(mappedBy = "authors")
-	private List<Book> books = new ArrayList<>();
-	
+	/////////////// getters and setters ///////////////////
+/*
+	public List<AuthorBook> getBooks() {
+		if (this.books == null) {
+			this.books = new ArrayList<>();
+		}
+		return this.books;
+	}*/
 	
 	
 
