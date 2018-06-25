d4699311666affd26c7faaae1a85d15b1666d2d0
darco2018 <us3cki@zoho.com>
2018-06-25 15:38:40 +0200

    Add test welcome message

--- src/main/java/pl/ust/bookshop/HomeController.java
+++ src/main/java/pl/ust/bookshop/HomeController.java
@@ -1,15 +1,19 @@
 package pl.ust.bookshop;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HomeController {
 	
-	@RequestMapping("test")
+	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
+	@RequestMapping("/test") // http://localhost:8081/bookshop/test
 	public String test() {
-		return "test passed";
+		return "Test passed. " + welcomeMessage;
 		
 	}
 
