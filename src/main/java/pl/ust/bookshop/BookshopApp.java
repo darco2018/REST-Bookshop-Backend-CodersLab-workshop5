b402c086cd060183e2919e4ccd68d570c1eeac33
darco2018 <us3cki@zoho.com>
2018-06-28 12:16:12 +0200

    Beautify commented code in BookshopApp.java

--- src/main/java/pl/ust/bookshop/BookshopApp.java
+++ src/main/java/pl/ust/bookshop/BookshopApp.java
@@ -4,28 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+
+/*
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Component;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;*/
+
 @SpringBootApplication
 public class BookshopApp {
 	
-	
-	
-	
-	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(BookshopApp.class, args);
-		System.err.println(ctx); // AnnotationConfigServletWebServerApplicationContext
-	}
-	
-	/* UNCOMMENT ON THE FIRST RUN (PersistenceContext not loaded when @WebMvcTest is used in tests)
-	 
-	    import javax.persistence.EntityManager;
-		import javax.persistence.PersistenceContext;
-	 
-	  @PersistenceContext
-		private EntityManager entityManager;
-	 
+/* //UNCOMMENT ON THE FIRST RUN TO POPULATE DB (Commented off because PersistenceContext not loaded when @WebMvcTest is used in tests)
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@Component
 	@Order(Ordered.HIGHEST_PRECEDENCE)
-	 class DBPopulatorRunner implements ApplicationRunner {
+	class DBPopulatorRunner implements ApplicationRunner {
 
 		@Override
 		@Transactional // !!!
@@ -34,9 +33,19 @@ public class BookshopApp {
 			DBPopulator.populateAuthors(entityManager);
 			DBPopulator.populateBooks(entityManager);
 		}
-		
-	} 
-	*/
+
+	}*/
+	
+	
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(BookshopApp.class, args);
+		System.err.println(ctx); // AnnotationConfigServletWebServerApplicationContext
+	}
+	
+	
+	 
+	 
+	
 	
 	
 	
